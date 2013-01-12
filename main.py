f7318068eff2e0016832aa4e25b5de4c84556fd4
Richard Peng <peng.richard@gmail.com>
2013-01-12 06:59:03 -0500

    lowered pic ctr

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class ProcessHandler(BaseHandler):
         #self.write("Processing...")
         api = facebook.GraphAPI(self.current_user["access_token"])
         reference = urllib2.urlopen(self.get_argument("src")).read()
-        components = get_photo_array(api, maxPhotos = 150)
+        components = get_photo_array(api, maxPhotos = 100)
         result = process_image(reference, components)
         self.set_header("Content-Type", "image/jpg")
         self.write(result)
