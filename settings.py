fc443fbc294ec7097284cf2679a18f865af8631f
Richard Peng <peng.richard@gmail.com>
2014-05-20 00:04:12 -0400

    get env secrets

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
+import os
 # Keys
-facebook_app_key = ""
-facebook_app_secret = ""
-cookie_secret = ""
+facebook_app_key = os.environ.get("APP_ID", "")
+facebook_app_secret = os.environ.get("APP_SECRET", "")
+cookie_secret = os.environ.get("COOKIE_SECRET", "")
